aa30dcf20b8eb784713497e1981f69b9550e254e
unknown <abdelqader.fadi.h@gmail.com>
2022-10-28 19:24:52 +0200

    init projet

---
+++ src/main/java/com/devops/HealthyLife/HealthyLifeApplication.java
@@ -0,0 +1,13 @@
+package com.devops.HealthyLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyLifeApplication.class, args);
+	}
+//he
+}
