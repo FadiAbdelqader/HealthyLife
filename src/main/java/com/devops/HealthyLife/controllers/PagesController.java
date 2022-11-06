69d29c9104313f5e9954cc040081e05cd61ad403
unknown <abdelqader.fadi.h@gmail.com>
2022-11-06 15:25:00 +0100

    solve merge conflicts

--- src/main/java/com/devops/HealthyLife/controllers/PagesController.java
+++ src/main/java/com/devops/HealthyLife/controllers/PagesController.java
@@ -1,6 +1,5 @@
 package com.devops.HealthyLife.controllers;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,35 +12,4 @@ public class PagesController {
 	public String home() {
 		return "hello world";
 	}
-=======
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.devops.HealthyLife.modeles.Dessert;
-import com.devops.HealthyLife.modeles.Entree;
-import com.devops.HealthyLife.modeles.Plat;
-import com.devops.HealthyLife.modeles.Repas;
-import com.devops.HealthyLife.modeles.Sport;
-
-
-
-@Controller
-@ResponseBody
-public class PagesController {
-
-	
-	@RequestMapping("/")
-	public String repas() throws SQLException {
-	
-		return "hello";
-	}
-
->>>>>>> agenda
 }
